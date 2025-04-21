cda80e147e6f3dbd8332aaa550d87bc78fc6239f
Patrick Punch <punchp@oregonstate.edu>
2025-04-21 10:15:47 -0700

    updated index.js to use sql db for message storage.

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
+/** 
+ * Patrick Punch
+ * 4/21/2025
+ * CS 493
+*/
 require('dotenv').config();
+
+const mysql = require('mysql2/promise');
+
+const mysql_host = process.env.MYSQL_HOST || 'localhost';
+const mysql_port = process.env.MYSQL_PORT || '3306';
+const mysql_db = process.env.MYSQL_DATABASE || 'messagesdb';
+const mysql_user = process.env.MYSQL_USER || 'messagesuser';
+const mysql_password = process.env.MYSQL_PASSWORD;
+
+const mysqlPool = mysql.createPool({
+    connectionLimit: 10,
+    host: mysql_host,
+    port: mysql_port,
+    database: mysql_db,
+    user: mysql_user,
+    password: mysql_password
+});
+
 
 const express = require('express')
 const app = express();
@@ -7,57 +30,78 @@ app.use(express.json());
 
 const port = 8086;
 
-let next_id = 0;
-const messages = {};
 
 app.listen(port, () => {
     console.log(`== Server is listening on port ${port}`);
 });
 
-app.get("/messages", (req, res, next) => {
+app.get("/messages", async (req, res, next) => {
+    const [messages] = await mysqlPool.query("SELECT id, message FROM messages");
     res.send(messages);
     console.log("== Retrieved all messages");
 });
 
-app.get("/messages/:id", (req, res, next) => {
+app.get("/messages/:id", async (req, res, next) => {
     const id = req.params.id;
-
-    if (!(id in messages)) {
+    const [message] = await mysqlPool.query(`select id, message FROM messages
+        where id = ?
+    `, id);
+    if (message.length === 0){
         res.status(404).send({"error": `Message ${id} not found`});
-        console.log(`== Index ${id} out of bounds`);
+        console.log(`== Message ${id} not found`);
     } else {
-        res.send(messages[id]);
+        res.send(message[0]);
         console.log(`== Retrieved from id ${id}`);
     }
 });
 
-app.post("/messages", (req, res, next) => {
-    const message = req.body;
-    const id = next_id++;
+app.post("/messages", async (req, res, next) => {
+    const { message } = req.body;
+    if (!message){
+        res.status(400).send({ error: "Message content required" });
+        return;
+    }
+    const [result] = await mysqlPool.query(
+        "INSERT INTO messages (message) VALUES (?)",
+        [message]
+    );
 
-    message["id"] = id;
-    messages[id] = message;
+    const insertedId = result.insertId;
 
-    res.status(201);
-
-    res.send({
-        "id": id,
-        "links": {
-            "self": `/messages/${id}`
+    res.status(201).send({
+        id: insertedId,
+        links: {
+            self: `/messages/${insertedId}`
         }
     });
 
-    console.log(`== Posted to id ${id}`);
+    console.log(`== Inserted message id ${insertedId}`);
 });
 
-app.delete('/messages/:id', (req, res, next) => {
+app.delete('/messages/:id', async (req, res, next) => {
     const id = req.params.id;
 
-    if (!(id in messages)) {
-        res.status(404).send({"error": `Message ${id} not found`});
-        console.log(`== Index ${id} out of bounds`);
+    const [result] = await mysqlPool.query(
+        "DELETE FROM messages WHERE id = ?",
+        [id]
+    );
+
+    if (result.affectedRows === 0) {
+        res.status(404).send({ error: `Message ${id} not found` });
+        console.log(`== Message ${id} not found`);
     } else {
-        delete messages[id];
-        res.status(200).send({"status": `message ${id} deleted`});
+        res.status(200).send({ status: `Message ${id} deleted` });
+        console.log(`== Deleted message id ${id}`);
     }
 });
+
+async function init_db() {
+    await mysqlPool.query("drop table if exists messages");
+
+    await mysqlPool.query(`create table messages (
+        id integer primary key auto_increment,
+        message varchar(512)
+    )`);
+}
+
+init_db();
